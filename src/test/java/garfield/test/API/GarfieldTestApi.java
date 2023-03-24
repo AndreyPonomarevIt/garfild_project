7c23186621d6a5b437869daaecd726200eab7eee
AndreyPonomarev <Andrey.Ponomarev.it@gmail.com>
2023-03-24 22:05:13 +0300

    Add GarfieldPage, GarfieldStep, GarfieldTestApi, GarfieldTestUI, GarfieldText and Unit classes

--- src/test/java/garfield/test/API/GarfieldTestApi.java
+++ src/test/java/garfield/test/API/GarfieldTestApi.java
@@ -1,21 +1,35 @@
 package garfield.test.API;
 
 import garfield.test.UI.GarfieldTestUI;
+import garfield.test.main.page.GarfieldPage;
+import garfield.test.main.text.GarfieldText;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GarfieldTestApi extends GarfieldTestUI {
     @Test
-    public void testPostGarfieldNotCorrectDate() {
+    public void testPostGarfieldCorrectDate() {
            String body = "{\"STATE\":\"SUCCESS\"}";
         String url = "https://garfield.by/";
             given().queryParam("?login=yes").contentType("application/json").body(body).
                     when().post(url + "?login=yes").
                     then().assertThat().statusCode(200).body(equalTo(body));
+    }
 
-        }
-
+    @Test
+    public void testPostGarfieldNotCorrectDate() {
+        WebDriver driver = new ChromeDriver();
+        String body = "{\"STATE\":\"FAILURE\",\"INCORRECT_FIELDS\":[\"email\",\"password\"]}";
+        String url = "https://garfield.by/";
+        given().queryParam("?login=yes").contentType("application/json").body(body).
+                when().post(url + "?login=yes").
+                then().assertThat().statusCode(200).body(equalTo(body));
+    }
 
     @Test
     public void testPostGarfieldInputProduct() {
@@ -23,6 +37,5 @@ public class GarfieldTestApi extends GarfieldTestUI {
                 given().when()
                 .post(url).
                 then().statusCode(200).log().body();
-
     }
 }
