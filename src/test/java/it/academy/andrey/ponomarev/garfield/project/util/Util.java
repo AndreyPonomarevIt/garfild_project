a5cc276f96bdf6f55d63b417eeb1f618c0bfae34
AndreyPonomarev <Andrey.Ponomarev.it@gmail.com>
2023-04-02 12:57:15 +0300

    to check project

--- src/test/java/it/academy/andrey/ponomarev/garfield/project/util/Util.java
+++ src/test/java/it/academy/andrey/ponomarev/garfield/project/util/Util.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+
 import java.util.concurrent.TimeUnit;
 
 import static it.academy.andrey.ponomarev.garfield.project.constants.Constants.EXPLICITLY;
